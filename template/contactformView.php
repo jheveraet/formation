36a71ca38da26367724f0caa59e54ea2198ef650
vincent <vincent@kollebolle.com>
2017-12-13 17:31:08 +0100

    ajout header et footer sur form ajout contact

--- template/contactformView.php
+++ template/contactformView.php
@@ -1,3 +1,4 @@
+<?php include("header.php"); ?>
 <h1>Ajouter un contact </h1>
 <form method="post" action="createController.php">
   <label>prenom</label>: <input type="text" name="prenom" /><br />
@@ -5,3 +6,4 @@
   <label>age</label>: <input type="text" name="age" /><br />
   <input type="submit" value="ajouter" />
 </form>   
+<?php include("footer.php"); ?>
